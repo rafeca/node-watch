ceaa0f3b27b18350883a4bbe66e3a4535e51118f
Jorrit Duin <jorrit.duin@gmail.com>
2011-05-25 15:00:53 +0200

    initial import

---
+++ Jakefile
@@ -0,0 +1,58 @@
+desc("run specs");
+task("test",function(){
+	// Jasmine is cool
+	var jasmine = require('./deps/jasmine-node');
+	var Path= require('path')
+	var specFolder = Path.join(process.cwd(), "spec");
+
+	for (var key in jasmine)
+	  global[key] = jasmine[key];
+	
+	var isVerbose = false;
+	var showColors = true;
+	var extentions = "js";
+	jasmine.loadHelpersInFolder(specFolder, new RegExp("[-_]helper\\.(" + extentions + ")$"));
+	jasmine.executeSpecsInFolder(specFolder, function(runner, log){
+	  if (runner.results().failedCount == 0) {
+		process.exit(0);
+	  } else {
+		process.exit(1);
+	  }
+	}, isVerbose, showColors, new RegExp(".spec\\.(" + extentions + ")$", 'i'));
+
+});
+desc("Autorun specs");
+task('autotest',function(){
+	console.log('Quite autotest with CTRL - C');
+	var watch = require('./deps/watch/watch.js');
+	
+	var spawn = require('child_process').spawn;
+	var busy = false;
+	var test = null;
+	
+	watch.addDir("./spec").addDir("./lib/watch").onChange(function(file){
+		if(!busy){
+			busy = true;
+			
+			test    = spawn('jake', ['test']);
+	
+			test.stdout.on('data', function (data) {
+				process.stdout.write(data);
+			});
+			
+			test.stderr.on('data', function (data) {
+			 	process.stderr.write( data);
+			});
+			test.on('exit', function (code) {
+			  // Clear test (possible memory leak?)
+			  test.stdout.removeAllListeners("data");
+			  test.stderr.removeAllListeners("date");
+			  test.removeAllListeners("exit");
+			  test = null;
+			  busy = false;
+			});
+		}
+	});
+	
+
+});
---
+++ spec/watchSpec.js
@@ -0,0 +1,131 @@
+// File: ./spec/watchSpec.js
+/*
+TODO:
+make the test faster by mocking the async callbacks
+
+*/
+var fs = require("fs"); 
+var watch = require("../lib/watch/watch.js"); 
+
+
+describe('watch module test adding files', function(){
+	it('should be an object', function(){
+  		expect(typeof watch).toEqual("object");
+ 	}); 
+ 	it('has event properties', function(){
+ 		// http://nodejs.org/docs/v0.4.7/api/events.html#emitter.on
+  		expect(typeof watch.on).toEqual("function");
+	});
+		
+	it('should be possible to emit an event on file change', function(){
+		
+  		var fp1 = __dirname+"/tmp/file1.txt",
+  			stime = new Date().toUTCString(),
+  			event_detected = false, 
+  			counter = 0;
+  			
+  		watch.watchFile(fp1).on("change",function(){
+  			counter ++;
+  			event_detected = true;
+  			stime = new Date().toUTCString();  			
+  		});
+  		// Wat for the trigger
+  		waitsFor(function() {
+  		    if(event_detected){
+  		    	if(counter>0){
+  		    		asyncSpecDone();
+  		    		expect(event_detected).toBe(true);
+  		    		
+  		    		watch.removeAllListeners("change");
+  		    		watch.unwatchFile(fp1);
+  		    		
+  		    		expect(watch.listeners("change").length).toBe(0);
+  		    		return true;
+  		    	}
+  		    }  	    
+  		    return false;
+    	}, "The event is not detected",10000);
+    	expect(watch.listeners("change").length).toBe(1);
+  		asyncSpecWait.timeout = 10 * 1000;
+  		asyncSpecWait();
+    	fs.writeFileSync(fp1, stime);
+	}); 
+
+});
+
+describe('watch module test adding dirs', function(){
+	
+	it('should be possible to add dirs to watch', function(){
+		expect(typeof watch.addDir).toEqual("function");
+		// This dir does not excists (relative form process.cwd())
+		expect(function(){watch.addDir("./no_dirs")}).toThrow();
+		// This should be allright
+		expect(function(){watch.addDir(__dirname+"/tmp")}).not.toThrow();
+		// There should be 1 entry in de dirs array
+		expect(watch._helperGetWatchedDirs().length).toBe(1);
+		// Add a second dir (the same) which sould not be added
+		expect(function(){watch.addDir(__dirname+"/tmp")}).not.toThrow();
+		// Still one
+		expect(watch._helperGetWatchedDirs().length).toBe(1);
+	
+	}); 
+	it('should be possible to remove dirs to watch', function(){
+		expect(typeof watch.remDir).toEqual("function");
+		// Remove a non - excisting dir
+		expect(function(){watch.remDir("./no_dirs")}).toThrow();
+		expect(function(){watch.remDir(__dirname+"/tmp")}).not.toThrow();
+		expect(watch._helperGetWatchedDirs().length).toBe(0);	
+	}); 
+	
+	it('should emit a change on a watched dir', function(){
+		var event_detected = false, 
+			fp1 = __dirname+"/tmp/file1.txt",
+			stime = new Date().toUTCString(),
+			changed_file = "",
+  			counter = 0;
+  			
+		watch.addDir(__dirname+"/tmp").onChange(function(file,prev,curr){
+  			counter ++;
+  			event_detected = true;
+  			stime = new Date().toUTCString();  	
+  			changed_file = file;	
+  		});
+  		
+  		waitsFor(function() {
+  		    if(event_detected){
+  		    	if(counter>0){
+  		    		asyncSpecDone();
+  		    		expect(fp1).toBe(changed_file);
+  		    		
+  		    		watch.clearListeners();
+  		    		watch.remDir(__dirname+"/tmp");
+  		    		expect(watch.listeners("change").length).toBe(0);
+  		    		return true;
+  		    	}
+  		    }  	    
+  		    return false;
+    	}, "The event is not detected",10000);
+    	
+    	asyncSpecWait.timeout = 10 * 1000;
+  		asyncSpecWait();
+    	fs.writeFileSync(fp1, stime);
+	}); 
+	
+});
+
+describe('watch module chainabiliy test', function(){
+	
+	it('should return THIS on method calls for chainability',function(){
+		var test1 = watch.addDir(__dirname+"/tmp");
+		expect(test1).toBe(watch);
+		var test2 = watch.remDir(__dirname+"/tmp");
+		expect(test2).toBe(watch);
+		var test3 = watch.onChange(function(file,prev,curr){
+			// NOTHING
+		});
+		expect(test3).toBe(watch);
+		var test4 = watch.clearListeners();
+		expect(test4).toBe(watch);
+	});
+	
+});
